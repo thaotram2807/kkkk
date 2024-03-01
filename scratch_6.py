49759f32c218174b1006bd43471c263b0725d81b
thaotram2807 <161817407+thaotram2807@users.noreply.github.com>
2024-03-01 16:36:22 +0700

    Update scratch_6.py

--- scratch_6.py
+++ scratch_6.py
@@ -1,10 +1,23 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+# Data Set
+data = pd.read_csv("scratch_6.csv")
+# Minimizing Dataset
+albany_df = data[data['region']=="Albany"]
+al_df = albany_df[albany_df["year"]==2015]
+# Bar graph
+bar_graph = px.bar(
+    al_df,
+    title = "Bar Graph",
+    x = "Date",
+    y = "Large Bags"
+)
+st.plotly_chart(bar_graph)
+#Bar Color
 bar_graph = px.bar(
 x = al_df["Date"],
 y = al_df["Large Bags"],
 title = "Bar Graph",
 color=al_df["Large Bags"]
 )
-st.plotly_chart(bar_graph)
